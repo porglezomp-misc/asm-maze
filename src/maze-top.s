e9600fa06a278d8b5cc1f86ed3a7f65e0a83644f
Caleb Jones <code@calebjones.net>
2016-08-01 14:05:23 +0000

    Add collisions with the maze

--- src/maze-top.s
+++ src/maze-top.s
@@ -4,6 +4,11 @@ GIGA = 1000000000
 NS_PER_FRAME = GIGA/30
 ESC = 1
 
+UP = 103
+LEFT = 105
+RIGHT = 106
+DOWN = 108
+
 	.globl _start
 _start:
 	sub	sp, #4
@@ -19,12 +24,52 @@ _start:
 
 	ldr	r4, =kbd_keys
 	ldr	r11, =map
+	mov	r9, #260
+	mov	r10, #260
 mainloop:
 update:
 	bl	kbd_poll
+
+	mov	r5, r10
+	ldrb	r0, [r4, #UP]
+	cmp	r0, #0
+	addne	r10, #-32
+	ldrb	r0, [r4, #DOWN]
+	cmp	r0, #0
+	addne	r10, #32
+
+	lsr	r6, r9, #8
+	lsr	r7, r10, #8
+	add	r6, r7, LSL #4
+	ldrb	r6, [r11, r6]
+	cmp	r6, #0
+	movne	r10, r5
+
+	mov	r5, r9
+	ldrb	r0, [r4, #LEFT]
+	cmp	r0, #0
+	addne	r9, #-32
+	ldrb	r0, [r4, #RIGHT]
+	cmp	r0, #0
+	addne	r9, #32
+
+	lsr	r6, r9, #8
+	lsr	r7, r10, #8
+	add	r6, r7, LSL #4
+	ldrb	r6, [r11, r6]
+	cmp	r6, #0
+	movne	r9, r5
+
 draw:
 	mov	r0, #0
 	bl	fb_clear_color
+
+	mov	r2, #8
+	add	r0, r2, r9, LSR #5
+	add	r1, r2, r10, LSR #5
+	mov	r2, #2
+	mov	r3, #2
+	bl	draw_rect
 
 	mov	r5, #0xFF
 drawloop:
